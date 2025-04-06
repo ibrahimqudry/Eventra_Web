6cd0d889a80ec4b0680a7e1b03af7ff2f1ee2f21
Miramaged802 <miramaged345@gmail.com>
2025-04-06 14:58:46 +0200

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import About from './Pages/About'
-import Contact from './Pages/Contact'
-import Navbar from './componats/Nav'
-import Signin from './Pages/Signin'
-import Createevent from './pages/Createevent'
-import Events from './Pages/Events'
-import Servses from './Pages/Servses'
-import Footer from './componats/Footer'
-import EventDetails from './Pages/EventDetails'
-import UserDashboard from './Pages/UserDashboard'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Navbar from "./componats/Nav";
+import Signin from "./Pages/Signin";
+import Createevent from "./pages/Createevent";
+import Events from "./Pages/Events";
+import Servses from "./Pages/Servses";
+import Footer from "./componats/Footer";
 
 function App() {
   return (
@@ -18,15 +15,13 @@ function App() {
       <Routes>
         <Route path="/Nav" element={<Navbar />} />
         <Route path="/" element={<Home />} />
-        <Route path="/user" element={<UserDashboard/>} />
-         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} /> 
-         <Route path="/signin" element={<Signin />} /> 
-        <Route path='/Create' element={<Createevent />} />
-        <Route path='/events' element={<Events />} />
-        <Route path='/events-details' element={<EventDetails/>} />
-        <Route path='/Servses' element={<Servses />} /> 
-        <Route path='/Footer' element={<Footer />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/Create" element={<Createevent />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/Servses" element={<Servses />} />
+        <Route path="/Footer" element={<Footer />} />
       </Routes>
     </BrowserRouter>
   );
