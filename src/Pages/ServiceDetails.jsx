4b59b3dc83ac21c04b07287da0fc184399ea9924
ibrahimqudry <ibrahimqudry@gmail.com>
2025-05-03 19:02:23 +0300

    rm

--- src/Pages/ServiceDetails.jsx
+++ src/Pages/ServiceDetails.jsx
@@ -7,7 +7,7 @@ import Footer from '../components/Footer';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import '../css/serviceDetails.css';
+import '../css/ServiceDetails.css';
 
 const ServiceDetails = () => {
     const { id } = useParams();
