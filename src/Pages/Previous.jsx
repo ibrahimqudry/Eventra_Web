c1f639ed14947e8157efb2c1a768b32442f34064
ibrahimqudry <ibrahimqudry@gmail.com>
2025-05-03 19:03:57 +0300

    rm

--- src/Pages/Previous.jsx
+++ src/Pages/Previous.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../css/previousEventPage.css';
+import '../css/PreviousEventPage.css';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
