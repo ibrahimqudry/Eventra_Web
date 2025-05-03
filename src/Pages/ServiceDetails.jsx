40e609d071254dc98c85e0351840bd9926267de1
ibrahimqudry <ibrahimqudry@gmail.com>
2025-05-03 18:53:37 +0300

    add .css

--- src/Pages/ServiceDetails.jsx
+++ src/Pages/ServiceDetails.jsx
@@ -7,7 +7,7 @@ import Footer from '../components/Footer';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import '../css/serviceDetails';
+import '../css/serviceDetails.css';
 
 const ServiceDetails = () => {
     const { id } = useParams();
